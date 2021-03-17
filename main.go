03556a7e79aeed168ee74fdc1bf8b82a80efe8f6
ashon <ashon8813@gmail.com>
2021-03-18 01:11:31 +0900

    Add user api UserProfile kind
    logs:
    
    kubebuilder create api --group user --version v1alpha1 --kind UserProfile

--- main.go
+++ main.go
@@ -74,6 +74,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "User")
 		os.Exit(1)
 	}
+	if err = (&controllers.UserProfileReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("UserProfile"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "UserProfile")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
